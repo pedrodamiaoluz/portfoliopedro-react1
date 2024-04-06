6f5f9fef1e8106458ad0dee9cf1789918d37f751
Pedro Damiao <121252897+pedrodamiaoluz@users.noreply.github.com>
2024-04-06 16:30:43 +0000

    Atualizei

--- React/src/components/TopoSite/style.js
+++ React/src/components/TopoSite/style.js
@@ -4,9 +4,10 @@ import styled from "styled-components";
 export const Container = styled.div`
     height: 80vh;
     display: flex;
+    flex-direction: column-reverse;
     justify-content: center;
     align-items: center;
-    gap: 90px;
+    
 ` 
 
 export const conteudo = styled.div`
